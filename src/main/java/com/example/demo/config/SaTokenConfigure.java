2560e6184bc26596044dd87391d0826e063fdc26
deasdania <dea.dania.business@gmail.com>
2025-02-06 13:22:57 +0700

    adding redis config too

--- src/main/java/com/example/demo/config/SaTokenConfigure.java
+++ src/main/java/com/example/demo/config/SaTokenConfigure.java
@@ -4,11 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import com.example.demo.satoken.SaTokenBeanInjection;
+import com.example.demo.satoken.StpInterfaceImpl;
+
+import cn.dev33.satoken.application.ApplicationInfo;
+import cn.dev33.satoken.config.SaTokenConfig;
+import cn.dev33.satoken.dao.SaTokenDaoRedisJackson;
+import cn.dev33.satoken.log.SaLog;
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
+import cn.dev33.satoken.log.SaLog;
+import cn.dev33.satoken.log.SaLogForConsole;
+import cn.dev33.satoken.spring.SaBeanInject;
+import cn.dev33.satoken.spring.SaTokenContextForSpring;
+import cn.dev33.satoken.spring.json.SaJsonTemplateForJackson;
 import cn.dev33.satoken.stp.StpLogic;
 
+// import org.redisson.api.RedissonClient;
 
 /**
  * [Sa-Token 权限认证] 配置类 
@@ -18,6 +33,10 @@ import cn.dev33.satoken.stp.StpLogic;
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
 
+
+    @Value("${sa.route.prefix}")
+    private String routePrefix;
+
 	/**
 	 * 注册 Sa-Token 拦截器打开注解鉴权功能  
 	 */
@@ -26,7 +45,12 @@ public class SaTokenConfigure implements WebMvcConfigurer {
 		// 注册 Sa-Token 拦截器打开注解鉴权功能 
 		registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
 	}
-	
+
+	@Bean
+    public SaLog saLog() {
+        return new SaLogForConsole(); // Ensure SaLog has a no-argument constructor
+    }
+
     /**
      * Sa-Token 整合 jwt 
      */
@@ -35,4 +59,40 @@ public class SaTokenConfigure implements WebMvcConfigurer {
     	return new StpLogicJwtForSimple();
     }
     
+    @Bean
+    public SaTokenBeanInjection saTokenBeanInjection(
+            SaLog log,
+            SaTokenConfig config,
+            RedisConnectionFactory connectionFactory
+    ) {
+        return new SaTokenBeanInjection(log, config, connectionFactory, routePrefix);
+    }
+
+    @Bean
+    public SaBeanInject saBeanInject(
+            SaLog log,
+            SaTokenConfig config,
+            RedisConnectionFactory connectionFactory
+    ) {
+        SaBeanInject inject = new SaBeanInject(log, config);
+
+        // 基于 Spring 的上下文处理器
+        inject.setSaTokenContext(new SaTokenContextForSpring());
+
+        // 基于 Jackson 的 json解析器
+        inject.setSaJsonTemplate(new SaJsonTemplateForJackson());
+
+        // 基于 Jackson 序列化的 Redis 持久化组件
+        SaTokenDaoRedisJackson saTokenDaoRedisJackson = new SaTokenDaoRedisJackson();
+        saTokenDaoRedisJackson.init(connectionFactory);
+        inject.setSaTokenDao(saTokenDaoRedisJackson);
+
+        // 权限和角色数据
+        inject.setStpInterface(new StpInterfaceImpl());
+
+        // 项目路由前缀，方便路由拦截鉴权的
+        ApplicationInfo.routePrefix = routePrefix;
+
+        return inject;
+    }
 }
